4779b58c734c8ae3e205f0c747d00fdc0ab6e620
iourisorokine <isorokine@gmail.com>
2019-11-01 11:37:57 +0100

    boilerplate and mongo d

--- src/index.js
+++ src/index.js
@@ -7,8 +7,30 @@ import initializeDb from './db';
 import middleware from './middleware';
 import api from './api';
 import config from './config.json';
+import mongoose from 'mongoose';
 
-let app = express();
+require("dotenv").config();
+
+mongoose
+  .connect("mongodb://localhost/spend-smart-database", {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+const app_name = require("../package.json").name;
+// const debug = require("debug")(
+//   `${app_name}:${path.basename(__filename).split(".")[0]}`
+// );
+
+
+const app = express();
 app.server = http.createServer(app);
 
 // logger

46e47288be5ca265d3fa97f98e82d35b226a3021
iourisorokine <isorokine@gmail.com>
2019-11-29 11:12:10 +0100

    auto-generate selectable monts

--- client/src/components/BudgetGraph.js
+++ client/src/components/BudgetGraph.js
@@ -1,51 +1,122 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Doughnut } from "react-chartjs-2";
+import { Select, InputLabel, MenuItem } from "@material-ui/core";
 
 const months = [
-    'January', 'February', 'March', 'April', 'May',
-    'June', 'July', 'August', 'September',
-    'October', 'November', 'December'
-    ];
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
-const getAmountsPerCat= (cats, data) =>{
-    const amountsPerCat = cats.map(cat=>{
-        return data.reduce((acc,val)=>{
-            if(val.category===cat) return acc + val.amount;
-            return acc;
-        },0)
-    })
-    return amountsPerCat;
-}
+const getAmountsPerCat = (cats, data) => {
+  const amountsPerCat = cats.map(cat => {
+    return data.reduce((acc, val) => {
+      if (val.category === cat) return acc + val.amount;
+      return acc;
+    }, 0);
+  });
+  return amountsPerCat;
+};
 
-const filterDataByMonth = (data,month) =>{
-    const filtered = data.filter(spend => new Date(spend.date).getMonth()===month)
-    console.log(filtered)
-    return filtered
-}
+const filterDataByMonth = (data, month) => {
+  const filtered = data.filter(
+    spend => new Date(spend.date).getMonth() === month
+  );
+  console.log(filtered);
+  return filtered;
+};
 
 const BudgetGraph = props => {
-
+  const [monthToSelect, setMonthToSelect] = useState(0);
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth());
   let monthStr = months[selectedMonth];
-  const categories=["Food", "Accomodation", "Drinks", "Leisure", "Clothes", "Culture", "Transport"];  
-  let spendDataForSelectedMonth= filterDataByMonth(props.data.spends,selectedMonth);
+  const categories = [
+    "Food",
+    "Accomodation",
+    "Drinks",
+    "Leisure",
+    "Clothes",
+    "Culture",
+    "Transport"
+  ];
+
+  let spendDataForSelectedMonth = filterDataByMonth(
+    props.data.spends,
+    selectedMonth
+  );
   const amountsPerCat = getAmountsPerCat(categories, spendDataForSelectedMonth);
-  const totalSpend = spendDataForSelectedMonth.reduce((acc, val) => acc + val.amount, 0)
+  const totalSpend = spendDataForSelectedMonth.reduce(
+    (acc, val) => acc + val.amount,
+    0
+  );
+
   const graphData = {
-    datasets: [{
+    datasets: [
+      {
         data: amountsPerCat,
         // extra colors for user generated categories
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#e803b9","#c45800","#c45850","#e803b9","#c45800"],
-    }],
-    labels: categories,
-};
+        backgroundColor: [
+          "#3e95cd",
+          "#8e5ea2",
+          "#3cba9f",
+          "#e8c3b9",
+          "#c45850",
+          "#e803b9",
+          "#c45800",
+          "#c45850",
+          "#e803b9",
+          "#c45800"
+        ]
+      }
+    ],
+    labels: categories
+  };
+
+  // automation of months display
+
+  const getMonthsToSelect = () => {
+    const spendMonthsArr = props.data.spends.map(spend =>
+      new Date(spend.date).getMonth()
+    );
+    const monthsToSelect = [];
+    months.forEach(month => {
+      if (spendMonthsArr.includes(months.indexOf(month))) {
+        monthsToSelect.push(
+          <MenuItem value={months.indexOf(month)}>{month}</MenuItem>
+        );
+      }
+    });
+    return monthsToSelect;
+  };
+  const monthsToRender = getMonthsToSelect();
 
   return (
     <div>
-      <button onClick={()=>setSelectedMonth(9)}>filter</button>
+      <div className="flex-row">
+        <InputLabel id="month">Select Month:</InputLabel>
+        <Select
+          labelId="month"
+          name="month"
+          value={monthToSelect}
+          onChange={e => setMonthToSelect(e.target.value)}>
+          {monthsToRender}
+        </Select>
+        <button onClick={() => setSelectedMonth(monthToSelect)}>filter</button>
+      </div>
       <h4>Your budget by categories:</h4>
       <Doughnut data={graphData} width={300} height={150}></Doughnut>
-      <p>Total Spend for {monthStr}: {totalSpend} EUR</p>
+      <p>
+        Total Spend for {monthStr}: {totalSpend} EUR
+      </p>
     </div>
   );
 };
--- client/src/components/EditSpend.js
+++ client/src/components/EditSpend.js
@@ -1,11 +1,123 @@
-import React from 'react'
+import React, { useState } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  FormGroup,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  TextField
+} from "@material-ui/core";
+import { styles } from "../styles/GlobalMUIStyles";
+import axios from "axios";
 
 const EditSpend = props => {
-    return (
-        <div>
-            <p>here is edit spend {props.match.params}</p>
-        </div>
-    )
-}
+  const { classes } = props;
+  const [spendName, setSpendName] = useState("");
+  const [spendDate, setSpendDate] = useState(new Date());
+  const [spendAmount, setSpendAmount] = useState(0);
+  const [spendCategory, setSpendCategory] = useState("");
 
-export default EditSpend
+  const handleChange = e => {
+    const { name, value } = e.target;
+    if (name === "spendName") setSpendName(value);
+    if (name === "spendAmount") setSpendAmount(value);
+    if (name === "spendDate") setSpendDate(value);
+    console.log(name,value)
+  };
+
+  const selectCategory = e => {
+    const buttons = Array.from(
+      document.getElementsByClassName("category-option")
+    );
+    buttons.forEach(el => el.classList.remove("option-selected"));
+    e.target.classList.toggle("option-selected");
+    setSpendCategory(e.target.innerText);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.setCreateSpend(false);
+    axios
+      .post("/api/spend", {
+        name: spendName,
+        amount: spendAmount,
+        date: spendDate,
+        category: spendCategory,
+        budgetId: props.budget._id
+      })
+      .then(response => {
+        props.getBudgetData();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <div className="create-spend">
+      <h2>Create New Spend</h2>
+      <form onSubmit={handleSubmit}>
+        <FormGroup>
+          <FormControl>
+            <InputLabel htmlFor="spendName">Name:</InputLabel>
+            <Input name="spendName" type="text" onChange={handleChange} />
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor="spendAmount">Amount:</InputLabel>
+            <Input
+              name="spendAmount"
+              min="0"
+              step="0.01"
+              type="number"
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl>
+            <TextField
+              name="spendDate"
+              label="Date:"
+              type="date"
+              className={classes.textField}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </FormControl>
+          <div className="category-options-container">
+            <div className="category-option" onClick={selectCategory}>
+              Food
+            </div>
+            <div className="category-option" onClick={selectCategory}>
+              Accomodation
+            </div>
+            <div className="category-option" onClick={selectCategory}>
+              Transport
+            </div>
+            <div className="category-option" onClick={selectCategory}>
+              Clothes
+            </div>
+            <div className="category-option" onClick={selectCategory}>
+              Culture
+            </div>
+            <div className="category-option" onClick={selectCategory}>
+              Leisure
+            </div>
+            <div className="category-option" onClick={selectCategory}>
+              Drinks
+            </div>
+          </div>
+          <Button className={classes.buttonBlueGrad} type="submit">
+            Create
+          </Button>
+          <p onClick={() => props.setCreateSpend(false)} className="black-link">
+            Cancel
+          </p>
+        </FormGroup>
+      </form>
+    </div>
+  );
+};
+
+export default withStyles(styles)(EditSpend);

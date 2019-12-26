ee8c1d6a0076ec69db9e4099db544cd72235914c
iourisorokine <isorokine@gmail.com>
2019-12-26 13:14:11 +0100

    view bug fix 1

--- src/index.js
+++ src/index.js
@@ -36,6 +36,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// View engine
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
 // 3rd party middleware
 app.use(cors({
 	exposedHeaders: config.corsHeaders
